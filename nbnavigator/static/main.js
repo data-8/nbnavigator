badf73feec142a57cf877779f77f7dcb21131ed2
alinxie@berkeley.edu <alinxie@berkeley.edu>
2017-03-24 17:25:28 -0700

    hello world

--- nbnavigator/static/main.js
+++ nbnavigator/static/main.js
@@ -12,12 +12,23 @@ define([
             'This is the current notebook application instance:',
             Jupyter.notebook
         );
+        var tab_id = 'nbnavigator'
+        $('<div/>')
+            .attr('id', tab_id)
+            .addClass('tab-pane')
+            .text('hello world')
+            .appendTo('.tab-content');
+
          $("#tabs").append(
             $('<li>')
             .append(
                 $('<a>')
+                .attr('href', '#' + tab_id)
                 .attr('data-toggle', 'tab')
                 .text('nbnavigator')
+                .on('click', function (evt) {
+                    window.history.pushState(null, null, '#' + tab_id);
+                })
             )
         );
     }
